ce6a2d868881f13890c2a645e66f2dfb2f9c2616
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-06-17 16:15:35 -0700

    Working on completing product_of_all_other_numbers using pip and numpy.

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,13 +1,12 @@
-import numpy
 '''
 Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
-    for i in range(len(arr) - 1):
-        arr[i] = numpy.prod(arr[:i]) * numpy.prod(arr[i + 1:])
-    return arr
+
+    pass
+
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
